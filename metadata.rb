5cdc8085034f4c6b15b5ea11079d2596b942cb5d
Tiago Sampaio <tiago@flugel.it>
2017-05-01 15:10:16 -0300

    Added tests and fixed file locations

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,15 @@
-name "git"
+name "rabbitmq_flugel"
 version "0.1.0"
+maintainer 'Flugel'
+maintainer_email 'tiago@flugel.it'
+description "Installs and TEST rabbitmq configuration"
+
+recipe 'rabbitmq', "Install standalone rabbitmq"
+
+depends 'erlang'
+depends 'yum-epel'
+depends 'yum-erlang_solutions'
+depends 'dpkg_autostart'
+depends 'rabbitmq'
+
+supports 'centos', '>= 7.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,41 @@
 package "git"
+package 'logrotate'
+package 'socat'
 
-log "Well, that was too easy"
+include_recipe "yum-epel"
+include_recipe 'erlang'
+include_recipe 'rabbitmq'
+
+package "python"
+package "python2-pip"
+package "python2-pika"
+
+node.default['rabbitmq']['enabled_plugins'] = ['rabbitmq_management']
+
+node['rabbitmq']['enabled_plugins'].each do |plugin|
+  rabbitmq_plugin plugin do
+    action :enable
+    notifies :restart, "service[#{node['rabbitmq']['service_name']}]"
+  end
+end
+
+node['rabbitmq']['disabled_plugins'].each do |plugin|
+  rabbitmq_plugin plugin do
+    action :disable
+  end
+end
+
+template '/tmp/consumer.py' do
+  source 'consumer.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+template '/tmp/sender.py' do
+  source 'sender.erb'
+  owner 'root'
+  group 'root'
+  mode '0755'
+end
+
+log "Base packages installed"
